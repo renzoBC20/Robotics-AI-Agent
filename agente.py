9f283f034c853d7e18d21999f94322fadb85e586
RenzoB <renzofabian20@gmail.com>
2025-01-19 23:05:29 -0600

    tarea

--- agente.py
+++ agente.py
@@ -8,6 +8,13 @@ import asyncio
 class MyMessageType:
     content: str
 
+class MyAgent2(RoutedAgent):
+    def __init__(self) -> None:
+        super().__init__("MyAgent2")
+
+    @message_handler
+    async def handle_my_message_type(self, message: MyMessageType, ctx: MessageContext) -> None:
+        print(f"Hola, soy {self.id.type}, recibi este mensaje: {message.content}")
 
 class MyAgent(RoutedAgent):
     def __init__(self) -> None:
@@ -15,14 +22,16 @@ class MyAgent(RoutedAgent):
 
     @message_handler
     async def handle_my_message_type(self, message: MyMessageType, ctx: MessageContext) -> None:
-        print(f"{self.id.type} received message: {message.content}")
+        await self.send_message(MyMessageType(f"Hola, soy {self.id.type}, recibi este mensaje: {message.content}"), AgentId("Agente2", "default"))
 
 runtime = SingleThreadedAgentRuntime()
 
 async def main():
-    await MyAgent.register(runtime, "my_agent", lambda: MyAgent())
+    await MyAgent.register(runtime, "Agente1", lambda: MyAgent())
+    await MyAgent2.register(runtime, "Agente2", lambda: MyAgent2())
     runtime.start()  # Start processing messages in the background.
-    await runtime.send_message(MyMessageType("Hello, World!"), AgentId("my_agent", "default"))
+    mensaje = input("Ingrese un mensaje: ")
+    await runtime.send_message(MyMessageType(mensaje), AgentId("Agente1", "default"))
     await runtime.stop()  # Stop processing messages in the background.
 
 if __name__ == "__main__":
